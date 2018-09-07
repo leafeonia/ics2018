b75a89c7db24dfa05f2cd8bc19890db80d2811d0
tracer-ics2018 <tracer@njuics.org>
2018-09-07 18:11:26 +0000

    >  compile 171860575 leafeon Linux d1e0e27acfb5 4.4.27-moby #1 SMP Wed Oct 26 14:21:29 UTC 2016 x86_64 GNU/Linux  18:11:26 up 1 day, 15:49,  4 users,  load average: 0.00, 0.00, 0.00 c57f20369691f4744344c28fbdfbe37dc33bebb

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -43,16 +43,16 @@ static int cmd_x(char *args){
 	printf("running.\n");
 	char* arg1 = strtok(args," ");
 	char* arg2 = strtok(NULL," ");
-	/*if(arg2){
+	if(arg2){
 		int length = atoi(arg1);
-		int address = atoi(arg2);
+		int address = atoi(arg2+2);
 		for(int i = 0;i < length;++i){
-			printf("0x%08x:0x%08x\n",)
+			printf("0x%08x:0x%08x\n",address,vaddr_read(address,4));
+			address+= 4;
 		}
-	}*/
-	printf("0x%08x\n",paddr_read(0x100000,8));
-	int temp = atoi(arg2+2);
-	printf("%s %d\n",arg1,temp);
+	}
+	printf("0x%08x\n",vaddr_read(0x100000,4));
+	//printf("%s %d\n",arg1,temp);
 	return 0;
 }
 
