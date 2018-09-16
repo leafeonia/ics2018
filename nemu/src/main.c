c6b06be300ed36426fbca8ddd4f98286610e4d6b
171860575-Mao Yiming <maoym@126.com>
2018-09-16 11:07:13 +0800

    PA1.2

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
+  //the code for expression evaluation correctness examination is in ui.c(required by PA1.2)
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
