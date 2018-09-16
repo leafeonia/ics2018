edcae8f007de85df0d495784e2dda7d66128c5ed
tracer-ics2018 <tracer@njuics.org>
2018-09-16 10:04:51 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:04:51 up  7:36,  1 user,  load average: 0.57, 0.45, 0.37 8a2af0128ab20256d3f6244ed44cab5e97f0c872

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,14 +5,6 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
-  /*bool *success = (bool *)malloc(sizeof(bool));
-  char* arg = strtok(argv," ");
-  while(arg){
-  	printf("test = %u\n",expr(arg,success));
-  	arg = strtok(NULL," ");
-  	arg = strtok(NULL," ");
-  }
-  free(success);*/
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
