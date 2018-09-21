2b788c6d8b9b5bd71420d0cff56a69ed88cb13a0
tracer-ics2018 <tracer@njuics.org>
2018-09-21 14:48:21 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:48:21 up  3:33,  1 user,  load average: 0.20, 0.16, 0.08 436266badeb0276ef644a353a97c6d5933bf7602

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,4 +1,6 @@
 #include "monitor/expr.h"
+#include <stdlib.h>
+#include <sys/types.h>
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
@@ -6,8 +8,11 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
+  bool *success = (bool*)malloc(sizeof(bool));
+  printf("test:%u\n",expr("1+2",success));
+  free(success);
   //the code for expression evaluation correctness examination is in ui.c(required by PA1.2)
-
+  
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
