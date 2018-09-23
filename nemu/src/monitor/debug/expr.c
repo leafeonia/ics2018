90a0b1e7ef158e3fd231971564506296379561ee
tracer-ics2018 <tracer@njuics.org>
2018-09-23 09:51:33 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  09:51:33 up  9:44,  1 user,  load average: 0.16, 0.25, 0.12 67fbf1ef9e93ca68ec787d19ee8cfc7a715f4b3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -188,6 +188,7 @@ static bool make_token(char *e) {
 }
 
 bool checkparentheses(int p,int q){
+	printf("check(%d,%d)",p,q);
 	if(tokens[p].type != LEFT_BRACKET || tokens[q].type != RIGHT_BRACKET) return false;
 	int cnt = 0;
 	for(int i = p + 1;i < q;i++){
