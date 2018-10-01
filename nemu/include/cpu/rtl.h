0573a5148d67c9a28147c92e7f3d71b778f7e35c
tracer-ics2018 <tracer@njuics.org>
2018-10-01 23:09:20 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  23:09:20 up 14:36,  1 user,  load average: 0.17, 0.09, 0.04 5d88f3c0a12609e810e674a70a86ee73e64dd8

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -161,8 +161,11 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
+  printf("In rtl_push(), before sub, esp = %x\n",reg_l(R_ESP));
   rtl_subi(&reg_l(R_ESP),&reg_l(R_ESP),4);
+  printf("In rtl_push(), after sub, esp = %x\n",reg_l(R_ESP));
   rtl_mv(&reg_l(R_ESP),src1);
+  printf("In rtl_push(), after move, esp = %x\n",reg_l(R_ESP));
   return;
   
 }
