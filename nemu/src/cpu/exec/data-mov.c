31083a8cdd31472fbe38466736d3f53b27e35c7b
tracer-ics2018 <tracer@njuics.org>
2018-10-21 16:30:07 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:30:07 up 13:08,  5 users,  load average: 0.36, 0.22, 0.12 cc9c1fdc7b161dc0ff8371d84ffa8c2ad0dd2705

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -8,7 +8,7 @@ make_EHelper(mov) {
 
 make_EHelper(push) {
   rtl_push(&id_dest->val);
-
+   operand_write(id_dest, &id_dest->val);
   print_asm_template1(push);
   printf("---%s---\n",decoding.assembly);
 }
@@ -16,7 +16,7 @@ make_EHelper(push) {
 make_EHelper(pop) {
 	pr(&id_dest->val);
   rtl_pop(&id_dest->val);
-
+   operand_write(id_dest, &id_dest->val);
   print_asm_template1(pop);
   printf("---%s---\n",decoding.assembly);
 }
