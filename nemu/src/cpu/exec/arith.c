41c2041080885fd114642e5e550b942bfbbf41d6
tracer-ics2018 <tracer@njuics.org>
2018-10-23 15:40:49 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:40:49 up 20:45,  5 users,  load average: 0.56, 0.26, 0.13 a32abfa3fb828ae2a85cda0cd6f7d7341075880

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -3,6 +3,17 @@
 make_EHelper(add) {
   rtl_add(&id_dest->val,&id_dest->val,&id_src->val);
     operand_write(id_dest, &id_dest->val);
+    
+  rtl_setrelop(RELOP_LTU, &t0, &t2, &id_dest->val);
+  rtl_or(&t0, &t3, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0, &t0);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template2(add);
     printf("---%s---\n",decoding.assembly);
@@ -17,6 +28,16 @@ make_EHelper(sub) {
   //pr(&id_dest->type);
   operand_write(id_dest, &id_dest->val);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
+  
+  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &t2);
+  rtl_or(&t0, &t3, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
   //printf("esp = %x\n",cpu.esp);
   print_asm_template2(sub);
   printf("---%s---\n",decoding.assembly);
