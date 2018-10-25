7a6eb56257c00e5863875d5578911e9e92a42287
tracer-ics2018 <tracer@njuics.org>
2018-10-25 20:50:00 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:50:00 up 1 day,  6:15,  5 users,  load average: 0.08, 0.12, 0.09 c7a61f7b842914c29db712308bf87548bad6a91d

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -15,7 +15,7 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  printf("TODO\n\n\n\n\n\n\n");
+  _putc('F');//("TODO\n\n\n\n\n\n\n");
   return NULL;
 }
 
