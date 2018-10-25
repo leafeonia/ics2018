d524efd12a23581fc2bffb42a0d91c01dda4ae16
tracer-ics2018 <tracer@njuics.org>
2018-10-25 22:21:15 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:21:15 up 1 day,  7:46,  5 users,  load average: 0.10, 0.03, 0.01 db349903f2e97a3a74b922eefcc07f93ac73efe1

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,13 +1,21 @@
 #include "klib.h"
 #include "assert.h"
-#ifndef __ISA_NATIVE__
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+	size_t ans = 0;
+	while(*s){
+		s++;
+		ans++;
+	}
+    return ans;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+	char* temp = dst;
+	while(*src) *dst++ = *src++; 
+	*dst = '\0';
+	return temp;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
@@ -15,12 +23,21 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  _putc('F');//("TODO\n\n\n\n\n\n\n");
-  return NULL;
+    char* temp = dst;
+	while(*dst) dst++;
+	while(*src) *dst++ = *src++;
+	*dst = '\0';
+	return temp;
 }
 
-int strcmp(const char* s1, const char* s2) {
-  return 0;
+int strcmp(const char* s1, const char* s2) {  
+  while(*s1 && *s2 && (*s1 == *s2)){
+    s1++;
+	s2++;
+  }
+  if(*s1 > *s2) return 1;
+  else if(*s1 < *s2) return -1;
+  else return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
@@ -28,7 +45,11 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+	char* temp = (char*) v;
+	for(int i = 0;i < n;++i){
+		*temp++ = c;
+	}
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
@@ -36,7 +57,16 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+	char* temps1 = (char*) s1;
+	char* temps2 = (char*) s2;
+	n-=1;
+	while(*temps1 && *temps2 && (n--) && (*temps1 == *temps2)){
+		temps1++;
+		temps2++;
+	}
+	if(*temps1 > *temps2) return 1;
+	else if (*temps1 < *temps2)return -1;
+	else return 0;
 }
 
 #endif
