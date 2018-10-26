2779f9872e71b02365e6515848ae219e1857c80a
tracer-ics2018 <tracer@njuics.org>
2018-10-26 09:47:22 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  09:47:22 up 1 day,  9:15,  5 users,  load average: 0.52, 0.18, 0.06 8a68db513bb630d8e9b5a050b5571be18e69d3e

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -12,7 +12,48 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+    int ret = 0;
+    int cnt,num;
+    char a[100];
+    char* p;
+    va_list ap;
+    va_start(ap,fmt);
+    while(*fmt){
+        if(*fmt == '%'){
+            char op = *++fmt;
+            switch(op){
+                case 's':
+                    p = va_arg(ap,char*);
+                    while(*p){
+                        *out++ = *p++;
+                        ret++;
+                    }
+                    break;
+                case 'd':
+                    num = va_arg(ap,int);
+                    cnt = 0;
+                    while(num){
+                        a[++cnt] = num%10 + '0';
+                        num /= 10;
+                    }
+                    ;
+                    while(cnt){
+                        *out++ = a[cnt];
+                        cnt--;
+                        ret++;
+                    }
+                    break;
+                default:break;
+            }
+            fmt++;
+        }
+        else {
+            *out++ = *fmt++;
+            ret++;
+        }
+    }
+    *out = '\0';
+    return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
