2752a7ac3d2da5ac2456fb3e35f8d08dc0ebd98b
tracer-ics2018 <tracer@njuics.org>
2018-10-29 20:35:20 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:35:20 up  4:40,  5 users,  load average: 0.26, 0.16, 0.19 6d792eb42c538c1f5865aae137cd89b0b9239279

--- nemu/src/device/io/port-io.c
+++ nemu/src/device/io/port-io.c
@@ -20,10 +20,9 @@ static void pio_callback(ioaddr_t addr, int len, bool is_write) {
   int i;
   //printf("nr_map = %d\n",nr_map);
   for (i = 0; i < nr_map; i ++) {
-  //printf("enter. i = %d,maps[i].low = %d,maps[i].high = %d\n",i,maps[i].low
-  //,maps[i].high);
+  
     if (addr >= maps[i].low && addr + len - 1 <= maps[i].high) {
-      
+      printf("enter. i = %d,maps[i].low = %d,maps[i].high = %d\n",i,maps[i].low,maps[i].high);
       if (maps[i].callback != NULL) {
         maps[i].callback(addr, len, is_write);
         //printf("enter\n");
@@ -45,7 +44,7 @@ void* add_pio_map(ioaddr_t addr, int len, pio_callback_t callback) {
 }
 
 static inline uint32_t pio_read_common(ioaddr_t addr, int len) {
-//printf("addr = %x\n",addr);
+  //printf("addr = %x\n",addr);
   assert(addr + len - 1 < PORT_IO_SPACE_MAX);
   pio_callback(addr, len, false);		// prepare data to read
   switch (len) {
