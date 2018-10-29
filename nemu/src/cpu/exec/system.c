46ed43c7474ecf68c48114fbefc6433e688fa511
tracer-ics2018 <tracer@njuics.org>
2018-10-29 17:30:53 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:30:53 up  2:19,  5 users,  load average: 0.42, 0.30, 0.18 1f07172cc97548f1ac9a32d58f87fc17806bfb

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -42,7 +42,18 @@ make_EHelper(iret) {
 }
 
 make_EHelper(in) {
-  rtl_li(&id_dest->val,pio_read_l(0x3f8));
+	switch(id_src->width){
+		case 4:
+			rtl_li(&id_dest->val,pio_read_l(0x3f8));
+			break;
+		case 2:
+			rtl_li(&id_dest->val,pio_read_w(0x3f8));
+			break;
+		case 1:
+			rtl_li(&id_dest->val,pio_read_b(0x3f8));
+			break;
+	}
+  	pr(&id_dest->val);
   operand_write(id_dest,&id_dest->val);
 
   print_asm_template2(in);
