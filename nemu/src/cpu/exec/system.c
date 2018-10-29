be11e5d487f499efd91f951be93c23cef85241e8
tracer-ics2018 <tracer@njuics.org>
2018-10-29 21:31:28 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:31:28 up  5:36,  5 users,  load average: 0.26, 0.18, 0.13 68200af17f2a7287573cd01d7071d8c245b3bc5

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -67,7 +67,7 @@ printf("---%s---\n",decoding.assembly);
 }
 
 make_EHelper(out) {
-  pio_write_l((ioaddr_t)cpu.eax,id_src->val);
+  pio_write_l((ioaddr_t)id_dest->val,id_src->val);
 	
   print_asm_template2(out);
   printf("---%s---\n",decoding.assembly);
