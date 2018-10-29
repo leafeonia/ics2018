c138e366cf64ccd02c87d3f4efcd0e8389279a10
tracer-ics2018 <tracer@njuics.org>
2018-10-29 21:33:24 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:33:24 up  5:38,  5 users,  load average: 0.05, 0.13, 0.11 9a85e5de1414b4244044c5efc3322e996e826

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -52,7 +52,7 @@ pr(&id_src->val);
 			rtl_li(&id_dest->val,pio_read_w((ioaddr_t)id_src->val));
 			break;
 		case 1:
-		printf("ret = %d\n",pio_read_b((ioaddr_t)id_src->val));
+		//printf("ret = %d\n",pio_read_b((ioaddr_t)id_src->val));
 			rtl_li(&id_dest->val,pio_read_b((ioaddr_t)id_src->val));
 			break;
 	}
@@ -67,6 +67,7 @@ printf("---%s---\n",decoding.assembly);
 }
 
 make_EHelper(out) {
+  printf("data = %x\n",id_src->val);
   pio_write_l((ioaddr_t)id_dest->val,id_src->val);
 	
   print_asm_template2(out);
