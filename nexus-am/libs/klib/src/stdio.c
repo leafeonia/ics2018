87be3122b4d8bda54e1913a222892a509986aac4
tracer-ics2018 <tracer@njuics.org>
2018-10-30 14:27:00 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:27:00 up 10:02,  5 users,  load average: 0.40, 0.14, 0.09 883f38279a28f02c6dbcaf5dec9f3c7381b73ee

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,75 @@
 #include <stdlib.h>
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#include <stdarg.h>
+#include <stdio.h>
+
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+    int ret = 0;
+    int cnt,num;
+    char a[100];
+    char* p;
+    while(*fmt){
+        if(*fmt == '%'){
+            char op = *++fmt;
+            switch(op){
+                case 's':
+                    p = va_arg(ap,char*);
+                    while(*p){
+                        *out++ = *p++;
+                        ret++;
+                    }
+                    break;
+                case 'd':
+                    num = va_arg(ap,int);
+                    cnt = 0;
+                    while(num){
+                        a[++cnt] = num%10 + '0';
+                        num /= 10;
+                    }
+                    ;
+                    while(cnt){
+                        *out++ = a[cnt];
+                        cnt--;
+                        ret++;
+                    }
+                    break;
+                default:break;
+            }
+            fmt++;
+        }
+        else {
+            *out++ = *fmt++;
+            ret++;
+        }
+    }
+    *out = '\0';
+    return ret;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+    va_list aptr;
+    va_start(aptr, fmt);
+    int ret = vsprintf(out, fmt, aptr);
+    va_end(aptr);
+    return ret;
+}
+
+int printf(const char *fmt, ...) {
+    char temp[1000];
+    char* s = temp;
+    va_list aptr;
+    va_start(aptr, fmt);
+    int ret = my_vsprintf(s, fmt, aptr);
+    va_end(aptr);
+    for (; *s; s ++) {
+        _putc(*s);
+    }
+    return ret;
+}
+
+/*
 int printf(const char *fmt, ...) {
   char temp[1000];
   char* s = temp;
@@ -12,6 +81,7 @@ int printf(const char *fmt, ...) {
   }
   return ret;
 }
+
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
@@ -60,7 +130,7 @@ int sprintf(char *out, const char *fmt, ...) {
     }
     *out = '\0';
     return ret;
-}
+}*/
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
