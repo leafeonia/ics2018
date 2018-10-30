b04465eacd63e36231fb7eb095ffb64da2f53bb7
tracer-ics2018 <tracer@njuics.org>
2018-10-30 14:33:36 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:33:36 up 10:09,  5 users,  load average: 0.03, 0.05, 0.06 4de733ff373af94841811735c40b24d8df7e0737

--- nexus-am/apps/hello/hello.c
+++ nexus-am/apps/hello/hello.c
@@ -9,7 +9,7 @@ int main() {
   for (int i = 0; i < 10; i ++) {
     print("Hello World!\n");
   }
-  for(int i = 0;i < 10;i++){
+  for(int i = 0;i < 100;i++){
   	printf("%d\n",i);
   }
   return 0;
