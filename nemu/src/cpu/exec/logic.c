8b0028afd20c0e9b6f03c73fdc01857553aae02f
tracer-ics2018 <tracer@njuics.org>
2018-10-31 13:04:41 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:04:41 up 37 min,  4 users,  load average: 1.87, 1.90, 1.54 1a02b6d1972ccabcb375f1d732654e9b9a26652e

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -54,7 +54,7 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  rtl_sar(&id_dest->val,&id_dest->val,&id_src->val);
+  rtl_sar(&id_dest->val,&id_dest->val,&id_dest->val);
   operand_write(id_dest, &id_dest->val);
   // unnecessary to update CF and OF in NEMU
 
