de054a9abbad981fec859d3bf6135ca1eb0eecaf
tracer-ics2018 <tracer@njuics.org>
2018-10-31 19:34:25 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:34:25 up 24 min,  4 users,  load average: 0.63, 0.54, 0.50 7e5370c9edc4d342696cc21b1b159e461b91576

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -5,7 +5,7 @@
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
-    _putc('1');
+   // _putc('1');
       _KbdReg *kbd = (_KbdReg *)buf;
       kbd->keydown = 0;
       kbd->keycode = inl(0x60);
