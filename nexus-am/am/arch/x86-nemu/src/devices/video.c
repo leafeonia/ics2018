0412596e377dce4ef0fde6a2ae6a18b97fa22c6c
tracer-ics2018 <tracer@njuics.org>
2018-11-01 00:19:50 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:19:50 up  4:21,  4 users,  load average: 0.12, 0.19, 0.37 fd34b191464508fd9f8cf9e68d60bf1b88c22ab

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -25,8 +25,9 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
       
       
       int i;
+      int a = size;
       //int size = screen_width() ;* screen_height();
-      for (i = 0; i < size; i ++) fb[i] = i;
+      for (i = 0; i < a; i ++) fb[i] = i;
 	  /*for(i = 0;i < size;++i){
 	  	//fb[i+ctl->x*screen_width()+ctl->y] = *(ctl->pixels+i);
 	  	fb[]
