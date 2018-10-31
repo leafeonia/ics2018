aed173229a151f837b65d2393d6329f6afea67de
tracer-ics2018 <tracer@njuics.org>
2018-10-31 22:13:03 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:13:03 up  3:02,  4 users,  load average: 0.15, 0.12, 0.10 ee9b44543cf5d66bb79bb559bb41d529dddc594

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -8,9 +8,10 @@ static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
+    int ret = inl(0x100);
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 400;
-      info->height = 300;
+      info->width = ret >> 16;
+      info->height = ret & 0xffff;
       return sizeof(_VideoInfoReg);
     }
   }
@@ -30,7 +31,7 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
 		
 	  for(i = ctl->x;i < ctl->x + ctl->w;++i){
 	  	for(j = ctl->y;j < ctl->y + ctl->h;++j){
-	  		fb[i*screen_width()+j] = *(ctl->pixels+i*screen_width()+j);
+	  		fb[i*screen_width()+j] = *(ctl->pixels);
 	  	}
 	  }
 
