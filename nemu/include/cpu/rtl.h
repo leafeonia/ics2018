1f01887c38465b5a2709aa97c4bdd242c3f6b0bf
tracer-ics2018 <tracer@njuics.org>
2018-11-02 08:38:01 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  08:38:01 up  1:06,  4 users,  load average: 1.07, 0.36, 0.13 9efcbab96861e4c5146bbdebd5a090b3fa1fb000

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -154,7 +154,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   *dest = ~(*src1);
 }
 
-void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //pr(src1);
   bool msb = (*src1 >> (width*8 - 1)) & 1;
