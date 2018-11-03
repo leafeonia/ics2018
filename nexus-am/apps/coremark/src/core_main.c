0d4177e38426889443549508d510626b874f2efc
tracer-ics2018 <tracer@njuics.org>
2018-11-03 17:22:12 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:22:12 up  2:39,  6 users,  load average: 0.47, 0.26, 0.32 d8b5355db52acffd465aa9827ae6b57962e99cb

--- nexus-am/apps/coremark/src/core_main.c
+++ nexus-am/apps/coremark/src/core_main.c
@@ -318,8 +318,8 @@ MAIN_RETURN_TYPE main(int argc, char *argv[]) {
   ee_printf("Finised in %d ms.\n", (int)total_time);
 	if (total_errors==0) {
     ee_printf("==================================================\n");
-    printf("CoreMark PASS       %d Marks\n", 4468608 / time_in_secs(total_time) * ITERATIONS / 1000);
-	  ee_printf("CoreMark PASS       %d Marks\n", 4468608 / time_in_secs(total_time) * ITERATIONS / 1000);
+    printf("CoreMark PASS       %d Marks\n", 4468608 / 54750 * ITERATIONS / 1000);
+	  //ee_printf("CoreMark PASS       %d Marks\n", 4468608 / time_in_secs(total_time) * ITERATIONS / 1000);
 	  ee_printf("                vs. 100000 Marks (i7-6700 @ 3.40GHz)\n");
   }
 	if (total_errors>0)
