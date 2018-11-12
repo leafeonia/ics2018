09d08ffd8503d1dcbb182c727f71c2ba74472fd5
tracer-ics2018 <tracer@njuics.org>
2018-11-12 19:46:33 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:46:33 up  5:41,  6 users,  load average: 0.05, 0.08, 0.04 da1c46437399adaf40ee64668d25754fed95d

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,6 +6,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   uint32_t addr = cpu.idtr.base + NO*8;
+  printf("addr = %d\n",addr);
   uint32_t low = vaddr_read(addr,4);
   uint32_t high = vaddr_read(addr+12,4);
   uint32_t jp = (high << 16) & low;
