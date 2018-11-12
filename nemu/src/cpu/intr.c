3d09241a58de04b7358c6b4581f1a2c682817a1a
tracer-ics2018 <tracer@njuics.org>
2018-11-12 21:10:47 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:10:47 up  7:06,  6 users,  load average: 0.00, 0.05, 0.07 aa7ffa6352acd0b6c23565a0bf4d6c5df80ece2f

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -14,7 +14,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   uint32_t jp = (high << 16) | low;
   printf("jp = %d\n",jp);
  // rtl_push(&ret_addr);
-  rtl_j(addr);
+  rtl_j(jp);
  // cpu.eip = ret_addr;
 }
 
