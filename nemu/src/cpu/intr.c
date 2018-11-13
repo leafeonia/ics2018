171fd906a5f690626923547cb3a06e49de72303a
tracer-ics2018 <tracer@njuics.org>
2018-11-13 13:28:22 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:28:22 up 13:42,  6 users,  load average: 0.11, 0.07, 0.08 78b4ec80785865ed50318ecfb35f198950a5204

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,7 +9,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   uint32_t low = vaddr_read(addr,2);
   uint32_t high = vaddr_read(addr+6,2);
   uint32_t jp = (high << 16) | low;
-  cpu.eflags.val = (cpu.eflags.CF + 2 + (cpu.eflags.ZF << 6) + (cpu.eflags.SF << 7) + (cpu.eflags.IF << 9) + (cpu.eflags.OF << 11));
+  cpu.eflags.val = 6;//(cpu.eflags.CF + 2 + (cpu.eflags.ZF << 6) + (cpu.eflags.SF << 7) + (cpu.eflags.IF << 9) + (cpu.eflags.OF << 11));
   rtl_push(&cpu.eflags.val);
   rtl_push(&cpu.CS);
   rtl_push(&ret_addr);
