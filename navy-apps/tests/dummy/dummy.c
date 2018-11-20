815c651b776136969f98ef6c0b75d557d699a78e
tracer-ics2018 <tracer@njuics.org>
2018-11-20 19:10:17 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:10:17 up 20:08,  6 users,  load average: 0.03, 0.05, 0.01 9cfd89f0242efd25b2b2e24549f59da976dd8dd3

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
+#include <stdio.h>
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
+  printf("dummy.c\n");
   return _syscall_(SYS_yield, 0, 0, 0);
 }
