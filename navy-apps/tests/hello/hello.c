26c2978688362e8687a2ddbee7f78383d316f957
tracer-ics2018 <tracer@njuics.org>
2018-11-21 19:49:58 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:49:58 up 1 day,  1:40,  6 users,  load average: 0.42, 0.18, 0.11 f950e4ae6534ed642a1f5f7bad3bb1dbeae2aa0c

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,12 +3,14 @@
 
 int main() {
   write(1, "Hello World!\n", 13);
-  int i = 10;
+  char buffer[100];
+  int i = 2;
   volatile int j = 0;
   while (1) {
     j ++;
     if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      sprintf(buffer,"Hello World from Navy-apps for the %dth time!\n", i ++);
+      write(1,buffer,100);
       j = 0;
     }
   }
