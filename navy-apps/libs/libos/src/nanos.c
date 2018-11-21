8b6867d51634e62ce433b7231e7cc73213cb4193
tracer-ics2018 <tracer@njuics.org>
2018-11-21 19:17:07 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:17:07 up 1 day,  1:07,  6 users,  load average: 0.20, 0.12, 0.04 6d53c4a7251dd78537447d06ce686cb026bb0d4e

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -34,8 +34,9 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count){
   _syscall_(SYS_write,fd,(intptr_t)buf,count);
+  panic("1");
  // _exit(SYS_write);
-  return 0;
+  return ;
 }
 
 extern char  end;
