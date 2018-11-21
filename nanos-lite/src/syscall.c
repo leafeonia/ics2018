459dc859439815e41ae8875e9f1d04bea7d3682a
tracer-ics2018 <tracer@njuics.org>
2018-11-22 00:19:02 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:19:02 up 1 day,  2:10,  6 users,  load average: 0.32, 0.09, 0.06 f14028e7ee1e3cdecc96f255fb0d8a5df1ccfc4b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,12 +13,10 @@ _Context* do_syscall(_Context *c) {
     case(SYS_exit):_halt(0);break;
     case(SYS_write):
     	Log("SYS_write");
-    	//Log("a[1] = %d,a[2] = %x,a[3] = %d",a[1],&a[2],a[3]);
     	if(a[1] == 1 || a[1] == 2){
     		char* addr = (char*)a[2];
     		while(a[3]--) _putc(*addr++);
     	}
-    	//Log("SYS_write finish.");
     	break;
     	//panic("Unhandled syscall ID = %d %d %d %d", a[0],a[1],a[2],a[3]);
     case(SYS_brk):
