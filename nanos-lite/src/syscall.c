cd9b80fa715e61bdd4fba860e485fa3563e33e6a
tracer-ics2018 <tracer@njuics.org>
2018-11-21 20:01:26 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:01:26 up 1 day,  1:51,  6 users,  load average: 0.06, 0.15, 0.13 d00ab16e716fef4ba9da149bfccd4fd89e3798c9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,7 +14,6 @@ _Context* do_syscall(_Context *c) {
     case(SYS_write):
     	Log("SYS_write");
     	//Log("a[1] = %d,a[2] = %x,a[3] = %d",a[1],&a[2],a[3]);
-    	a[3]++;
     	if(a[1] == 1 || a[1] == 2){
     		char* addr = (char*)a[2];
     		while(a[3]--) _putc(*addr++);
