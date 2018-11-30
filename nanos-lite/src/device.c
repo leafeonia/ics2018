622f46c8dc6e9a04ed437154a9142f1da821f683
tracer-ics2018 <tracer@njuics.org>
2018-11-30 22:42:36 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:42:36 up  8:02,  6 users,  load average: 0.35, 0.17, 0.18 4448b2e4ee48490a9f1e85d2d5adb33cb4710dd

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,12 +2,9 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  for(int i = 0;i < len;i++){
-    _putc(((char*)buf)[i]);
-  } 
-  return len;
-  //strncpy((char*)0x3f8, buf, len);
-  //return strlen((char*)0x38);
+	int ret = len;
+	while(len--) _putc(*(char*)buf++);
+  	return ret;
 }
 
 #define NAME(key) \
@@ -18,86 +15,18 @@ static const char *keyname[256] __attribute__((used)) = {
   _KEYS(NAME)
 };
 
-//size_t input_read(uintptr_t reg, void *buf, size_t size);
 size_t events_read(void *buf, size_t offset, size_t len) {
-  int key = read_key();
-  bool down = false;
-  if(key & 0x8000){
-    key ^= 0x8000;
-    down = true;
-  }
-  if(key != _KEY_NONE){
-    if(down)
-      sprintf(buf, "%s %s\n", "kd", keyname[key]);
-    else
-      sprintf(buf, "%s %s\n", "ku", keyname[key]);
-    //printf("events_read:%s\n", buf);
-  }
-  else{
-    sprintf(buf, "t %d\n", uptime());
-  }
-  if(strlen(buf) > len){
-    printf("\33[1;31mERROR: In events_read: buf is too long.\33[0m\n");
-    assert(0);
-  }
-  ((char*)buf)[len] = '\0';
-  return strlen(buf);
-  //return 0;
+  return 0;
 }
 
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  /*
-  if(len >= 128)
-    len = 127;
-  */
-  for(int i = 0;i < len;i++){
-    ((char*)buf)[i] = dispinfo[offset+i];
-  }
-  ((char*)buf)[len] = '\0';
-  //return len;
-  //printf("dispinfo_read: len:%d\tstrlen:%d\n", len, strlen(buf));
-  return strlen(buf);
+  return 0;
 }
 
-//size_t video_read(uintptr_t reg, void *buf, size_t size);
-//void draw_rect(uint32_t *pixels, int x, int y, int w, int h);
-uint32_t buff[1<<16];
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  /*
-  uint32_t size[2];
-  //printf("fb_write: I'm here.\n");
-  video_read(1, size, 8);
-  */
-  //printf("fb_write: %p\n", buf);
-  //draw_rect((uint32_t*)buf, offset%(screen_width()), offset/(screen_width()), len, 1);
-  //draw_rect((uint32_t*)buf, offset%screen_width(), offset/(screen_width()), len, 1);
-  //draw_rect((uint32_t*)buf, 0, offset/(screen_width()), len, 1);
-  /*
-  for(int i = 0;i < len;i++){
-    printf("%d ", ((uint32_t*)buf)[i]);
-  }
-  printf("\n");
-  */
-  offset /= 4;
-  //char term = ((char*)buf)[len];
-  //((char*)buf)[len] = '\0';
-  /*
-  for(int i = 0;i < len;i++){
-    buff[i] = ((uint32_t*)buf)[i];
-  }
-  buff[len] = '\0';
-  */
-  //draw_rect((uint32_t*)buf, offset%screen_width(), offset/screen_width(), screen_width(), screen_height());
-  draw_rect((uint32_t*)buf, offset%screen_width(), offset/screen_width(), len/4, 1);
-  //((char*)buf)[len] = term;
-  //printf("fb_write: offset:%#x\n", offset);
-  //draw_sync();
-  //printf("fb_write: len:%d\n", len);
-  //printf("fb_write: size0f(buf):%d\n", sizeof(buf));
-  return len;
-  //return 0;
+  return 0;
 }
 
 void init_device() {
@@ -106,10 +35,4 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-  /*
-  uint32_t size[2];
-  video_read(1, size, 8);
-  memset(dispinfo, 0, sizeof(dispinfo));
-  */
-  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
