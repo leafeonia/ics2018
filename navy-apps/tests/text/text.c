46cc9cfee7ce4c0ce547fc1adad0295fc6fd67c1
tracer-ics2018 <tracer@njuics.org>
2018-11-30 19:35:21 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:35:21 up  5:12,  6 users,  load average: 0.32, 0.23, 0.18 4a2def3e29c09b0db023d5026f95c3c642e7bf4c

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -13,11 +13,7 @@ int main() {
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    printf("i = %d,n = %d\n",i,n);
-    //if(n != i+1){
-    	//printf("assert(n == i + 1) fails because i = %d.n = %d\n",i,n);
-    	//assert(n == i + 1);
-    //}
+    assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
