5ad9a677067fd7d29f0b6437e52afe6c285308f4
tracer-ics2018 <tracer@njuics.org>
2018-11-30 20:25:08 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:25:08 up  6:02,  6 users,  load average: 0.22, 0.22, 0.13 879138c2d63ddec13dcdfd290a1054b20e26b2b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -16,7 +16,6 @@ enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-size_t serial_write(const void *buf, size_t offset, size_t len); 
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -31,8 +30,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0,invalid_read, invalid_write},
-  {"stdout", 0, 0, 0,invalid_read, serial_write},
-  {"stderr", 0, 0, 0,invalid_read, serial_write},
+  {"stdout", 0, 0, 0,invalid_read, invalid_write},
+  {"stderr", 0, 0, 0,invalid_read, invalid_write},
 #include "files.h"
 };
 
