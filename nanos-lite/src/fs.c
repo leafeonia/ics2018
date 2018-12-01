f3bef47e6d6da81d06f2597d5dca3ca55a287664
tracer-ics2018 <tracer@njuics.org>
2018-12-01 14:21:43 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:21:43 up 11:16,  6 users,  load average: 0.70, 0.51, 0.49 3d7afda8b90f8f09403cc324536dc27e36f21b46

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,6 +64,7 @@ void init_fs() {
 
 
 int fs_open(const char* pathname,int flags,int mode){
+printf("file = %s\n",pathname);
 	int i;
 	for(i = 0;i < NR_FILES;i++){
 		if(strcmp(pathname,file_table[i].name) == 0){
