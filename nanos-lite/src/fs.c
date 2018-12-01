6573e635d2028e4cffaf29c5783f27cc70a454e9
tracer-ics2018 <tracer@njuics.org>
2018-12-01 22:37:21 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:37:21 up 14:10,  6 users,  load average: 0.12, 0.08, 0.18 e74a94bec47741f12aa807dadcc8e8b91c5dee5a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -72,18 +72,23 @@ size_t fs_filesz(int fd){
 	return file_table[fd].size;
 }
 
-/*
+
 size_t fs_read(int fd, void* buf, size_t len){
 	//assert(file_table[fd].open_offset+len <= file_table[fd].size);
 	//printf("read address:%d\n",file_table[fd].disk_offset+file_table[fd].open_offset);
 	//printf("read value: %s\n",(char*)buf);
 	//printf("fs_read returns %d\n",ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len));
-	size_t ret;
-	if(file_table[fd].read != NULL) ret = file_table[fd].read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
-	else ret = ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+	size_t ret; //actually read num of byte
+	if(file_table[fd].open_offset + len > file_table[fd].size){
+		ret = file_table[fd].size - file_table[fd].open_offset;
+	}
+	else ret = len;
+	if(file_table[fd].read != NULL) file_table[fd].read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,ret);
+	else ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,ret);
 	file_table[fd].open_offset += ret;
 	return ret;
-}*/
+}
+/*
 size_t fs_read(int fd, void *buf,  size_t len){
   //printf("fs_read: I enter the function.\n");
   if(file_table[fd].read != NULL){
@@ -102,7 +107,7 @@ size_t fs_read(int fd, void *buf,  size_t len){
   //printf("fs_read: .oepn_offset:%d\n", file_table[fd].open_offset);
   //printf("HAHAHA\n");
   return len;
-}
+}*/
 
 
 size_t fs_write(int fd, const void *buf, size_t len){
