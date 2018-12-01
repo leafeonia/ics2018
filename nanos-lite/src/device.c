e32911683ca8e8dc839e9a8e63c6f37d891b42db
tracer-ics2018 <tracer@njuics.org>
2018-12-01 15:34:16 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:34:16 up 12:29,  6 users,  load average: 0.30, 0.33, 0.41 42ba38a85043e333e3b095ebf4599c08322a7ae7

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,21 +28,11 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   memcpy(buf,dispinfo+offset,len);
   return strlen(buf);
 }
-/*
-size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
-  for(int i = 0;i < len;i++){
-    ((char*)buf)[i] = dispinfo[offset+i];
-  }
-  ((char*)buf)[len] = '\0';
-  //return len;
-  //printf("dispinfo_read: len:%d\tstrlen:%d\n", len, strlen(buf));
-  return strlen(buf);
-}*/
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   //printf("enter fb_write\n");
-  draw_rect((uint32_t*)buf,offset%screen_width(),offset/4/screen_width(),len/4,1);	
+  draw_rect((uint32_t*)buf,offset/4%screen_width(),offset/4/screen_width(),len/4,1);	
   return len;
 }
 
