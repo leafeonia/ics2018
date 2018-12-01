5457b377985278f31df6ba184a529eb22e1c67a7
tracer-ics2018 <tracer@njuics.org>
2018-12-01 15:29:13 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:29:13 up 12:24,  6 users,  load average: 0.26, 0.42, 0.46 1fdf1b15d37821d68a589d0f11394c9df68bc0a

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,11 +25,7 @@ static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   printf("enter dispinfo_read\n");
-  /*for(int i = 0;i < len;i++){
-    ((char*)buf)[i] = dispinfo[offset+i];
-  }*/
   memcpy(buf,dispinfo+offset,len);
-  //((char*)buf)[len] = '\0';
   return strlen(buf);
 }
 /*
@@ -43,47 +39,14 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   //printf("dispinfo_read: len:%d\tstrlen:%d\n", len, strlen(buf));
   return strlen(buf);
 }*/
-/*
+
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   printf("enter fb_write\n");
   draw_rect((uint32_t*)buf,offset/4%screen_width(),offset/4%screen_height(),len/4,1);	
   return len;
-}*/
-size_t fb_write(const void *buf, size_t offset, size_t len) {
-  /*
-  uint32_t size[2];
-  //printf("fb_write: I'm here.\n");
-  video_read(1, size, 8);
-  */
-  //printf("fb_write: %p\n", buf);
-  //draw_rect((uint32_t*)buf, offset%(screen_width()), offset/(screen_width()), len, 1);
-  //draw_rect((uint32_t*)buf, offset%screen_width(), offset/(screen_width()), len, 1);
-  //draw_rect((uint32_t*)buf, 0, offset/(screen_width()), len, 1);
-  /*
-  for(int i = 0;i < len;i++){
-    printf("%d ", ((uint32_t*)buf)[i]);
-  }
-  printf("\n");
-  */
-  offset /= 4;
-  //char term = ((char*)buf)[len];
-  //((char*)buf)[len] = '\0';
-  /*
-  for(int i = 0;i < len;i++){
-    buff[i] = ((uint32_t*)buf)[i];
-  }
-  buff[len] = '\0';
-  */
-  //draw_rect((uint32_t*)buf, offset%screen_width(), offset/screen_width(), screen_width(), screen_height());
-  draw_rect((uint32_t*)buf, offset%screen_width(), offset/screen_width(), len/4, 1);
-  //((char*)buf)[len] = term;
-  //printf("fb_write: offset:%#x\n", offset);
-  //draw_sync();
-  //printf("fb_write: len:%d\n", len);
-  //printf("fb_write: size0f(buf):%d\n", sizeof(buf));
-  return len;
-  //return 0;
 }
+
+
 
 void init_device() {
   Log("Initializing devices...");
