fe0bc69f535f6c1596896f4e70351ca70345b87e
tracer-ics2018 <tracer@njuics.org>
2018-12-02 12:48:42 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  12:48:42 up 15:03,  6 users,  load average: 0.10, 0.18, 0.19 deaf253cca53e32443c07b2e317ff5d55aefb7d4

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -78,8 +78,9 @@ size_t fs_read(int fd, void* buf, size_t len){
 	//printf("read address:%d\n",file_table[fd].disk_offset+file_table[fd].open_offset);
 	//printf("read value: %s\n",(char*)buf);
 	//printf("fs_read returns %d\n",ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len));
-	size_t ret = len; //actually read num of byte
+	 //actually read num of byte
 	if(file_table[fd].read == NULL){
+		size_t ret = len;
 		if(file_table[fd].open_offset + len > file_table[fd].size)
 		ret = file_table[fd].size - file_table[fd].open_offset;
 		ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,ret);
