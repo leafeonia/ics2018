ac7e4bcae1d2ebf42e936ddb81167847f1f732f2
tracer-ics2018 <tracer@njuics.org>
2018-12-09 19:56:01 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:56:01 up  1:16,  2 users,  load average: 0.23, 0.14, 0.11 fc03f52bde7e745bb8212aad85e89e588572f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,40 +1,23 @@
 #include "proc.h"
-#include "common.h"
-
+#include "fs.h"
 #define DEFAULT_ENTRY 0x4000000
 
-//#define BUFF_SIZE 1024
-//char buff[BUFF_SIZE];
-
-size_t get_ramdisk_size();
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t fs_filesz(int fd);
-int fs_open(const char *pathname, int flags, int mode);
-size_t fs_read(int fd, void *buf, size_t len);
+size_t get_ramdisk_size();
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  //TODO();
-  int NO = fs_open(filename, 0, 0);
-  fs_read(NO, (void*)DEFAULT_ENTRY, fs_filesz(NO));
-  //printf("loader: I'm here.\n");
-  //ramdisk_read((void*)DEFAULT_ENTRY, 0, get_ramdisk_size());
-  //printf("lodder: get_ramdisk_size():%#x\n", get_ramdisk_size());
-  /*
-  size_t s = get_ramdisk_size();  
-  int i;
-  for(i = 0;(i+1)*BUFF_SIZE <= s;i++){
-    ramdisk_read(buff, i*BUFF_SIZE, BUFF_SIZE);
-    memcpy((void*)(DEFAULT_ENTRY+i*BUFF_SIZE), buff, BUFF_SIZE);
-  }
-  ramdisk_read(buff, i*BUFF_SIZE, s-BUFF_SIZE*i);
-  memcpy((void*)(DEFAULT_ENTRY+i*BUFF_SIZE), buff, s-BUFF_SIZE*i);
-  */
+  void* buf = (uintptr_t*)DEFAULT_ENTRY;
+  int fd = fs_open(filename,0,0);
+  //printf("fd = %d,fs_filesz(fd) = %d\n",fd,fs_filesz(fd));
+  fs_read(fd,buf,fs_filesz(fd));
+  fs_close(fd);
+  //ramdisk_read(buf,0,get_ramdisk_size());
   return DEFAULT_ENTRY;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   ((void(*)())entry) ();
-  //printf("naive_uload: I'm here\n");
 }
 
 void context_kload(PCB *pcb, void *entry) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,94 +1,54 @@
 #include "common.h"
 #include "syscall.h"
 #include "fs.h"
+#include "proc.h"
 
-char* NAME_OF_TASK[] = {
-  "SYS_exit",
-  "SYS_yield",
-  "SYS_open",
-  "SYS_read",
-  "SYS_write",
-  "SYS_kill",
-  "SYS_getpid",
-  "SYS_close",
-  "SYS_lseek",
-  "SYS_brk",
-  "SYS_fstat",
-  "SYS_time",
-  "SYS_signal",
-  "SYS_execve",
-  "SYS_fork",
-  "SYS_link",
-  "SYS_unlink",
-  "SYS_wait",
-  "SYS_times",
-  "SYS_gettimeofday"
-};
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+void naive_uload(PCB *pcb, const char *filename);
 
-intptr_t program_break;
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  //printf("do_syscall: I'm here.\n");
-  //printf("do_syscall: a:%s %d %d %d\n", NAME_OF_TASK[a[0]], a[1], a[2], a[3]);
-
-  /*
-  if(program_break == -1){
-    //printf("do_syscall: Enter break.\n");
-    extern intptr_t end;
-    program_break = end;
-  }
-  */
-
-  //printf("do_syscall: I'm here.\n");
-
   switch (a[0]) {
-    case SYS_exit: _halt(a[1]); return c;
-    case SYS_yield: _yield(); c->GPR1 = 0; return c;
-    case SYS_open:
-      c->GPR1 = fs_open((char*)a[1], (int)a[2], (int)a[3]);
-      return c;
-    case SYS_read:
-      c->GPR1 = fs_read((int)a[1], (void*)a[2], (size_t)a[3]);
-      return c;
-    case SYS_write:
-      c->GPR1 = fs_write((int)a[1], (void*)a[2], (size_t)a[3]);
-      //printf("do_syscall: SYS_write:%#x\n", c->GPR1);
-      return c;
-      /*
-      if(a[1] == 1 || a[1] == 2){
-        for(i = 0;i < a[3];i++){
-          _putc(((char*)a[2])[i]);       
-        }
-        c->GPR1 = i;
-        //a[0] = a[3];
-        //printf("do_syscall: SYS_write:1:%d\n", i);
-      }
-      else{
-        c->GPR1 = fs_write((int)a[1], (void*)a[2], (size_t)a[3]);
-      }
-      return c;
-      */
-    case SYS_close:
-      c->GPR1 = fs_close((int)a[1]);
-      return c;
-    case SYS_lseek:
-      c->GPR1 = fs_lseek((int)a[1], (size_t)a[2], (int)a[3]);
-      //printf("SYS_lseek: %d\n", c->GPR1);
-      return c;
-    case SYS_brk:
-      program_break = a[1];
-      c->GPR1 = 0;
-      //a[0] = 0;
-      /*
-      end = a[1];
-      a[0] = 0;
-      */
-      //panic("hhh\n");
-      return c;
+    case(SYS_yield):_yield();break;
+    case(SYS_exit):
+    	naive_uload(NULL,"/bin/init");
+    	break;
+    case(SYS_write):
+    	//Log("SYS_write");
+    	/*if(a[1] == 1 || a[1] == 2){
+    		char* addr = (char*)a[2];
+    		while(a[3]--) _putc(*addr++);
+    		c->GPRx = a[3];
+    	}*/
+    	c->GPRx = fs_write(a[1],(void*)a[2],a[3]);
+    	break;
+    	//panic("Unhandled syscall ID = %d %d %d %d", a[0],a[1],a[2],a[3]);
+    case(SYS_brk):
+    	c->GPRx = 0;
+    	break;
+    case(SYS_open):
+    	c->GPRx = fs_open((const char*)a[1],a[2],a[3]);
+    	//Log("SYS_open returns %d\n",c->GPRx);
+    	break;
+    case(SYS_read):
+    	c->GPRx = fs_read(a[1],(void*)a[2],a[3]);
+    	//Log("SYS_read returns %d\n",c->GPRx);
+    	break;
+    case(SYS_close):
+    	c->GPRx = fs_close(a[1]);
+    	//Log("SYS_close returns %d\n",c->GPRx);
+    	break;
+    case(SYS_lseek):
+    	c->GPRx = fs_lseek(a[1],a[2],a[3]);
+    	//Log("SYS_lseek returns %d\n",c->GPRx);
+    	break;
+    case(SYS_execve):
+    	naive_uload(NULL,(const char*)a[1]);
+    	break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
