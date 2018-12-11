57b3df5248bc3fbb59c2e9cf90d22579f06e52e2
tracer-ics2018 <tracer@njuics.org>
2018-12-11 23:47:04 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  23:47:04 up  4:32,  6 users,  load average: 0.02, 0.10, 0.08 1d3b7537b3b36eb60bdf5cfc2e7ac9489cdff0a

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ extern _Context* schedule(_Context *prev);//add
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
 	case _EVENT_YIELD:
-	//	printf("_EVENT_YIELD\n");
+		printf("_EVENT_YIELD\n");
 	//	break;
 		return schedule(c);
 	case _EVENT_SYSCALL:
