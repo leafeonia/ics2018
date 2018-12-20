8e44c581f7375379c5c2d9a1705bc3483e857865
tracer-ics2018 <tracer@njuics.org>
2018-12-20 13:00:17 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:00:17 up  6:19,  6 users,  load average: 0.17, 0.11, 0.10 25f25d0d31cf7a8ebf9994b4132f466386809cd

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -12,26 +12,26 @@ make_EHelper(lidt) {
 make_EHelper(mov_r2cr) {
   //operand_write(id_dest, &id_src->val);
   assert(id_dest->reg == 0 || id_dest->reg == 3);
-  printf("before mov_r2cr,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
-  printf("reg_l(%d) = %x\n",id_dest->reg,reg_l(id_dest->reg));
+  //printf("before mov_r2cr,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
+  //printf("reg_l(%d) = %x\n",id_dest->reg,reg_l(id_dest->reg));
   if(id_dest->reg == 0) cpu.CR0.val = reg_l(id_src->reg);
   else cpu.CR3.val = reg_l(id_src->reg);
-  printf("after mov_r2cr,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
+  //printf("after mov_r2cr,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
 //printf("id_dest->reg = %d\n",id_dest->reg);
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
-  printf("---%s---\n",decoding.assembly);
+  //printf("---%s---\n",decoding.assembly);
 }
 
 make_EHelper(mov_cr2r) {
   //operand_write(id_dest, &id_src->val);
   assert(id_src->reg == 0 || id_src->reg == 3);
-  printf("before mov_cr2r,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
+  //printf("before mov_cr2r,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
   if(id_src->reg == 0) rtl_li(&reg_l(id_dest->reg),cpu.CR0.val);
   else rtl_li(&reg_l(id_dest->reg),cpu.CR3.val);
-  printf("after mov_cr2r,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
+  //printf("after mov_cr2r,cpu.eax = %x,cpu.CR0.val = %x,cpu.CR3.val = %x\n",cpu.eax,cpu.CR0.val,cpu.CR3.val);
 //printf("id_src->reg = %d\n",id_src->reg);
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
-  printf("---%s---\n",decoding.assembly);
+  //printf("---%s---\n",decoding.assembly);
 
 #if defined(DIFF_TEST)
   difftest_skip_ref();
