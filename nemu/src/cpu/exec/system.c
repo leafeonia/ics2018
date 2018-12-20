9ffa38984ead940ce4c7e7c7faa2778c9ad68b36
tracer-ics2018 <tracer@njuics.org>
2018-12-20 11:32:18 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:32:18 up  5:27,  6 users,  load average: 0.12, 0.12, 0.09 edcc53305ce5e352d0abe305303282691931f8ae

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -13,14 +13,14 @@ make_EHelper(mov_r2cr) {
   operand_write(id_dest, &id_src->val);
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
-  printf("---%s---\n",decoding.assembly);
+  //printf("---%s---\n",decoding.assembly);
 }
 
 make_EHelper(mov_cr2r) {
   operand_write(id_dest, &id_src->val);
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
-  printf("---%s---\n",decoding.assembly);
+  //printf("---%s---\n",decoding.assembly);
 
 #if defined(DIFF_TEST)
   difftest_skip_ref();
