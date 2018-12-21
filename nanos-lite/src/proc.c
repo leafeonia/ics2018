87c734f7f93dac4c79e9e36ed919daabb7c6b7a6
tracer-ics2018 <tracer@njuics.org>
2018-12-21 18:51:04 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:51:04 up 20:12,  6 users,  load average: 0.12, 0.25, 0.24 b25b2227e7d38dbeedb776f6f1f2f5872a58b22

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
 	//naive_uload(NULL,"/bin/init");
 	
 	context_kload(&pcb[0],(void*)hello_fun);
-	context_uload(&pcb[0], "/bin/pal");
+	context_uload(&pcb[1], "/bin/pal");
 	
 	//printf("proc.c:in init_proc(): &hello_fun = %x\n",(void*)hello_fun);
 	switch_boot_pcb();
