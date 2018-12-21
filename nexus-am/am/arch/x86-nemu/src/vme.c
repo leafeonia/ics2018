e0aa4033a6f7a3b1cb0d6069c48df748e5d35dbe
tracer-ics2018 <tracer@njuics.org>
2018-12-21 13:23:53 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:23:53 up 16:18,  6 users,  load average: 0.11, 0.14, 0.10 75d5e20d8d7690801d0e9a9bfd3c9c523006258b

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -38,7 +38,7 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
       PTE pte = PGADDR(pdir_idx, 0, 0) | PTE_P;
       
       PTE pte_end = PGADDR(pdir_idx + 1, 0, 0) | PTE_P;
-      if(pdir_idx == 31) ret = (int)ptab;
+      if(pdir_idx == 31) ret = pte;
       for (; pte < pte_end; pte += PGSIZE) {
         *ptab = pte;
         ptab ++;
