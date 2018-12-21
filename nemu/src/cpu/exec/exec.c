5831c82066ebc7f3735178b1210bfa2747c6b201
tracer-ics2018 <tracer@njuics.org>
2018-12-21 21:17:38 +0800

    >  compile 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:17:38 up 22:31,  6 users,  load average: 0.26, 0.33, 0.34 c0b552b8e2d6b969384ef0874176035b420b89c

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -258,7 +258,7 @@ void exec_wrapper(bool print_flag) {
 #endif
   
   update_eip();
-  if(flag == 1)printf("cpu.INTR = %d, cpu.eflags.IF = %d\n",cpu.INTR,cpu.eflags.IF);
+ // if(flag == 1)printf("cpu.INTR = %d, cpu.eflags.IF = %d\n",cpu.INTR,cpu.eflags.IF);
   if (cpu.INTR & cpu.eflags.IF) {
     printf("\033[31m\033[1menter exec_wrapper\n\033[0m");
     cpu.INTR = false;
