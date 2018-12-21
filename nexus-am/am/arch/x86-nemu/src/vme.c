ec0ce183e5f0d7d86207a84e29044039b291501d
tracer-ics2018 <tracer@njuics.org>
2018-12-21 16:59:40 +0800

    >  run 171860575 leafeon Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:59:40 up 19:54,  6 users,  load average: 0.04, 0.02, 0.05 254c12b2dbe34999cf2370d58731a4e5c244ead

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -86,7 +86,7 @@ void _switch(_Context *c) {
 
 int _map(_Protect *p, void *va, void *pa, int mode) {
   PDE* updir = p->ptr;
-  uint32_t dir_idx = 4*PDX(va);
+  uint32_t dir_idx = PDX(va);
   PTE* uptab = NULL;
   int ret = 0;
   if((updir[dir_idx] & 1) == 0){  //&1 => present bit
@@ -97,7 +97,7 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
   }
   else uptab = (PTE*)(uptab[dir_idx] & 0xfffff000);//&0xfffff => base
   
-  uint32_t tab_idx = 4*PTX(va);
+  uint32_t tab_idx = PTX(va);
   ret = (int)(&updir[0x80]);
   uptab[tab_idx] = (((uintptr_t)pa & 0xfffff000) | PTE_P);
   //PTE pte = kpdirs[pdir_idx];
